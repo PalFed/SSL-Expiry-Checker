d5d37e4df81a07ef1aed5cdd32c8d66bf86ec2a7
PalFed <github@palfed.com>
2019-05-19 21:36:41 -0500

    Create sslChecker.ini.php

---
+++ sslChecker.ini.php
@@ -0,0 +1,11 @@
+[sslChecker]
+# When to turn orange (turns red when < 0)
+warnDays = 30
+
+# PHP Date format to show the expiry date.
+dateFormat = Y-m-d
+
+# Example domains - Change these to be your own domains
+domains[]="www.palfed.com"
+domains[]="www.co-funded.com"
+
